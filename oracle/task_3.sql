4f109a0f33257d91d94c70593f7b7d453caa826b
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2023-10-28 16:30:07 +0600

    changed task_3.sql

--- oracle/task_3.sql
+++ oracle/task_3.sql
@@ -1,18 +1,24 @@
 REM   Script: task_3
 REM   task_3
 
-create or replace view task_3 as
-select cust.cust_id, cust.fullname, sum(coalesce(tra.transaction_amt, 0) * coalesce(curr.exch_rate, 0)) as summa
-from customer cust
-left join transaction tra on tra.cust_id = cust.cust_id 
-left join currency_rate curr on curr.currency_id = tra.currency_id
-where sysdate >= cust.valid_from and sysdate < cust.valid_to
-  and tra.transaction_dt >= curr.valid_from and tra.transaction_dt < curr.valid_to
-  and to_char(tra.transaction_dt, 'mm.yyyy') = '01.2021'            -- за выбранный месяц
---tra.transaction_dt >= (sysdate - interval '1' month)   -- за последний месяц
-  and tra.currency_id = 'RUR'
-group by cust.cust_id, cust.fullname
-order by cust.fullname;
+create or replace view task_3 as 
+select tx.cust_id,
+    tx.tra_month,
+    sum(tx.tra_summa) as summa
+from (
+    select cust.cust_id,
+        to_char(tra.transaction_dt, 'yyyy-mm') as tra_month,
+        tra.transaction_amt * curr.exch_rate as tra_summa
+    from liga.customer cust
+        left join liga.transaction tra on tra.cust_id = cust.cust_id
+        left join liga.currency_rate curr on curr.currency_id::text = tra.currency_id::text
+    where sysdate >= cust.valid_from and sysdate < cust.valid_to 
+        and tra.transaction_dt >= curr.valid_from and tra.transaction_dt < curr.valid_to 
+) tx
+group by tx.cust_id, 
+        tx.tra_month
+order by tx.cust_id,
+         tx.tra_month;
 
 comment on table task_3 is 'Посчитать сумму транзакций клиента в рублях за месяц';
 
--- postgre/task_3.sql
+++ postgre/task_3.sql
@@ -1,14 +1,20 @@
-create or replace view liga.task_3 as
-select cust.cust_id, cust.fullname, sum(coalesce(tra.transaction_amt, 0) * coalesce(curr.exch_rate, 0)) as summa
-from liga.customer cust
-left join liga.transaction tra on tra.cust_id = cust.cust_id 
-left join liga.currency_rate curr on curr.currency_id = tra.currency_id
-where localtimestamp >= cust.valid_from and localtimestamp < cust.valid_to
-  and tra.transaction_dt >= curr.valid_from and tra.transaction_dt < curr.valid_to
-  and to_char(tra.transaction_dt, 'mm.yyyy') = '01.2021'            -- за выбранный месяц
---tra.transaction_dt >= (localtimestamp - interval '1' month)   -- за последний месяц
-  and tra.currency_id = 'RUR'
-group by cust.cust_id, cust.fullname
-order by cust.fullname;
+create or replace view liga.task_3 as 
+select tx.cust_id,
+    tx.tra_month,
+    sum(tx.tra_summa) as summa
+from (
+    select cust.cust_id,
+        to_char(tra.transaction_dt, 'yyyy-mm') as tra_month,
+        tra.transaction_amt * curr.exch_rate as tra_summa
+    from liga.customer cust
+        left join liga.transaction tra on tra.cust_id = cust.cust_id
+        left join liga.currency_rate curr on curr.currency_id::text = tra.currency_id::text
+    where localtimestamp >= cust.valid_from and localtimestamp < cust.valid_to 
+        and tra.transaction_dt >= curr.valid_from and tra.transaction_dt < curr.valid_to 
+) tx
+group by tx.cust_id, 
+        tx.tra_month
+order by tx.cust_id,
+         tx.tra_month;
 
 comment on view liga.task_3 is 'Посчитать сумму транзакций клиента в рублях за месяц';
